17ede124797784480145cd6acc2a6e6b2aaaca10
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-02-10 18:06:16 -0600

    Fix formatting

--- src/player/Player2D.cs
+++ src/player/Player2D.cs
@@ -8,11 +8,10 @@ public class Player2D: IPlayer {
         player = super;
     }
 
-	public void handleMovement(float delta) {
-		//TODO: Impliment movement physics for 2d
-	}
-
+    public void handleMovement(float delta) {
+        //TODO: Impliment movement physics for 2d
+    }
     public void updateCameraRotation() {
-        
+
     }
 }
