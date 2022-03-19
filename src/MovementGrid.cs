c61a72866610a6efe2f1815276de497a585c4bf5
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-03-19 13:34:11 -0500

    Add MovementGrid and tweak settings

---
+++ src/MovementGrid.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public class MovementGrid : Spatial {
+	public override void _Ready() {
+		
+	}
+ 	public override void _Process(float delta) {
+		 
+ 	}
+}
--- src/Settings.cs
+++ src/Settings.cs
@@ -1,13 +1,13 @@
 public class Settings {
-	public static float mouseSensitivity = 8;
+	public static float mouseSensitivity = 12;
 	public static float fov = 80;
-	public static float gravity = 30;
-	public static float walkSpeed = 10;
-	public static float sprintAdd = 6;
+	public static float gravity = 25;
+	public static float walkSpeed = 15;
+	public static float sprintAdd = 5;
 	public static float acceleration = 8;
 	public static float deceleration = 10;
-	public static float airControl = .3f;
-	public static float jumpHeight = 15;
+	public static float airControl = .5f;
+	public static float jumpHeight = 17.5f;
 	public const bool debug = true;
 
 	public static int maxJumps = 2;
