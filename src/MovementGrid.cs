518fb1bdba3a0dd722b2ac3f6b67642e1ef6f1cb
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-03-21 11:14:46 -0500

    Get closest point in MovementGrid

--- src/MovementGrid.cs
+++ src/MovementGrid.cs
@@ -8,4 +8,23 @@ public class MovementGrid : Spatial {
  	public override void _Process(float delta) {
 		 
  	}
+
+	public static Vector3 getClosestPoint(Vector3 pos) {
+		float x = applyRounding(pos.x);
+		float y = applyRounding(pos.y);
+		float z = applyRounding(pos.z);
+
+		return new Vector3(x,y,z);
+	}
+
+	public static Vector2 getClosestPoint(Vector2 pos) {
+		float x = applyRounding(pos.x);
+		float y = applyRounding(pos.y);
+		
+		return new Vector2(x,y);
+	}
+
+	public static float applyRounding(float old) {
+		return Mathf.Round(old/25)*25;
+	}
 }
